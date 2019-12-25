ad2427fb2c44832451c18536193ec2003a7dd7b0
dinhthuan <dinhthuanvan@gmail.com>
2019-12-25 17:18:14 +0700

    layout part 1

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -4,94 +4,54 @@ import 'dart:developer';
 
 void main() => runApp(new MyApp());
 
-class RandomEnglishWords extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return new RandomEnglishWordsState();
-  }
-}
-
-//state
-class RandomEnglishWordsState extends State<RandomEnglishWords> {
-  final _words = <WordPair>[];
-  final _checkedWords = new Set<WordPair>();
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    // TODO: implement build
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("list of english words".toUpperCase()),
-        actions: <Widget>[
-          new IconButton(
-            icon: new Icon(Icons.list),
-            onPressed: _pushToSavedWordsScreen,
-          )
-        ],
-      ),
-      body: new ListView.builder(itemBuilder: (context, index) {
-        if (index >= _words.length) {
-          _words.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_words[index], index);
-      }),
-    );
-  }
-
-  _pushToSavedWordsScreen() {
-    // print('touch');
-    final pageRoute = new MaterialPageRoute(builder: (context) {
-      final listTiles = _checkedWords.map((wordPair) {
-        return new ListTile(
-          title: new Text(
-            wordPair.asUpperCase,
-            style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-          ),
-        );
-      });
-      return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Checked words'),
-        ),
-        body: new ListView(
-          children: listTiles.toList(),
-        ),
-      );
-    });
-    Navigator.of(context).push(pageRoute);
-  }
-
-  Widget _buildRow(WordPair wordPair, int index) {
-    final textColor = index % 2 == 0 ? Colors.red : Colors.blue;
-    final isChecked = _checkedWords.contains(wordPair);
-    return new ListTile(
-      leading: new Icon(
-        isChecked ? Icons.check_box : Icons.check_box_outline_blank,
-        color: textColor,
-      ),
-      title: new Text(
-        wordPair.asUpperCase,
-        style: new TextStyle(fontSize: 18.0, color: textColor),
-      ),
-      onTap: () {
-        setState(() {
-          if (isChecked) {
-            _checkedWords.remove(wordPair);
-          } else {
-            _checkedWords.add(wordPair);
-          }
-        });
-      },
-    );
-  }
-}
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = new Container(
+      padding: const EdgeInsets.all(10.0),
+      child: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Container(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: new Text(
+                    "Programming tutorials channel 1",
+                    style: new TextStyle(
+                        fontSize: 18.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                new Text(
+                  "This channel contains tutorial videos in Flutter, React native, Reactjs",
+                  style: new TextStyle(fontSize: 16.0, color: Colors.grey[850]),
+                ),
+              ],
+            ),
+          ),
+          new Icon(Icons.favorite, color: Colors.red,),
+          new Text(
+            "100",
+            style: new TextStyle(fontSize: 16.0),
+          )
+          
+        ],
+      ),
+    );
     // TODO: implement build
     return new MaterialApp(
-        title: "Flutter app demo", home: new RandomEnglishWords());
+      title: "",
+      home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text('My Youtube channel'),
+          ),
+          body: new ListView(
+            children: <Widget>[
+              new Image.asset('images/samsungA20.png', fit: BoxFit.cover),
+              titleSection
+            ],
+          )),
+    );
   }
 }
