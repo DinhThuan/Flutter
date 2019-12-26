b0939f9b0b1b3f0a48dff925c60c293fd5828049
dinhthuan <dinhthuanvan@gmail.com>
2019-12-26 14:01:11 +0700

    layout10

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -2,47 +2,49 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
-class MainPage extends StatefulWidget {
-  final String title;
-  MainPage({this.title}) : super();
-
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return MainPageState();
-  }
-}
-
-class MainPageState extends State<MainPage> {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("GridView Example"),
-      ),
-      body: new GridView.extent(maxCrossAxisExtent: 150, mainAxisSpacing: 5.0, children: _buildGridTitles(27)),
-    );
-  }
-  
-}
-
-List<Widget> _buildGridTitles(numberOfTitles){
-  List<Container> containers = new List<Container>.generate(numberOfTitles, (int index){
-    final imageName = 'images/image${index+1}.png';
-    return new Container(
-      child: new Image.asset(imageName, fit: BoxFit.cover,),
-    );
-  });
-  return containers;
-}
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var card = new Card(
+      child: new Column(
+        children: <Widget>[
+          new ListTile(
+            leading: new Icon(Icons.account_box, color: Colors.blue,size: 26,),
+            title: new Text("Anton", style: new TextStyle(fontWeight: FontWeight.w400),),
+            subtitle: new Text("Software developr"),
+          ),
+          new Divider(color: Colors.blue, indent: 16.0,),
+          new ListTile(
+            leading: new Icon(Icons.email,color: Colors.blue, size: 26,),
+            title: new Text("dvthuan@gmail.com", style: new TextStyle(fontWeight: FontWeight.w400),),
+            subtitle: new Text("Software developr"),
+          ),
+          new ListTile(
+            leading: new Icon(Icons.phone,color: Colors.blue, size: 26,),
+            title: new Text("+84-0988985008", style: new TextStyle(fontWeight: FontWeight.w400),),
+            subtitle: new Text("Software developr"),
+          )
+        ],
+      ),
+    );
+    final sizeBox =  new Container(
+      margin: new EdgeInsets.only(left: 10.0, right: 10.0),
+      child: new SizedBox(
+        height: 250.0,
+        child: card,
+      ),
+    );
+    final center = new Center(
+      child: sizeBox,
+    );
     // TODO: implement build
     return new MaterialApp(
-      title: "",
-      home: new MainPage(title: "GridView of Images",)
-    );
+        title: "",
+        home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text("Card example"),
+          ),
+          body: center,
+        ));
   }
 }
