feff8fc75d2997ba4197613ce3f8a21c579df9b5
dinhthuan <dinhthuanvan@gmail.com>
2020-01-03 09:21:38 +0700

    project todo list

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -3,48 +3,39 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  _showSnackBar() {
+    final snackBar = new SnackBar(
+      content: new Text("This is a SnackBar"),
+      duration: new Duration(seconds: 8),
+      backgroundColor: Colors.green,
+      action: new SnackBarAction(
+        label: 'OK',
+        onPressed: () {
+          print("Press OK on SnackBar");
+        },
+      ),
+    );
+    // How to display Snackbar?
+    _scaffoldKey.currentState.showSnackBar(snackBar);
+  }
+
   @override
   Widget build(BuildContext context) {
-    var card = new Card(
-      child: new Column(
-        children: <Widget>[
-          new ListTile(
-            leading: new Icon(Icons.account_box, color: Colors.blue,size: 26,),
-            title: new Text("Anton", style: new TextStyle(fontWeight: FontWeight.w400),),
-            subtitle: new Text("Software developr"),
-          ),
-          new Divider(color: Colors.blue, indent: 16.0,),
-          new ListTile(
-            leading: new Icon(Icons.email,color: Colors.blue, size: 26,),
-            title: new Text("dvthuan@gmail.com", style: new TextStyle(fontWeight: FontWeight.w400),),
-            subtitle: new Text("Software developr"),
-          ),
-          new ListTile(
-            leading: new Icon(Icons.phone,color: Colors.blue, size: 26,),
-            title: new Text("+84-0988985008", style: new TextStyle(fontWeight: FontWeight.w400),),
-            subtitle: new Text("Software developr"),
-          )
-        ],
-      ),
-    );
-    final sizeBox =  new Container(
-      margin: new EdgeInsets.only(left: 10.0, right: 10.0),
-      child: new SizedBox(
-        height: 250.0,
-        child: card,
-      ),
-    );
-    final center = new Center(
-      child: sizeBox,
-    );
     // TODO: implement build
     return new MaterialApp(
         title: "",
         home: new Scaffold(
+          key: _scaffoldKey,
           appBar: new AppBar(
-            title: new Text("Card example"),
+            title: new Text("An example SnackBar"),
+            actions: <Widget>[
+              new IconButton(
+                icon: new Icon(Icons.info),
+                onPressed: _showSnackBar,
+              )
+            ],
           ),
-          body: center,
         ));
   }
 }
